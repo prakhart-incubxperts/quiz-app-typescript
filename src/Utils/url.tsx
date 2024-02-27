b667785a78ebe0d0c7ee1471f8d91f5ef023f264
prakhart <prakhart@incubxperts.com>
2024-02-27 11:02:50 +0530

    url

--- src/Utils/url.tsx
+++ src/Utils/url.tsx
@@ -1,4 +1,4 @@
-export const url="http://13.234.119.112:5000/api";
+export const url="http://13.232.154.99:5000/api";
 
 //pm2 start src/app.ts --interpreter ts-node --name quiz-app-node //node
 //pm2 serve build 8000 --spa //react
